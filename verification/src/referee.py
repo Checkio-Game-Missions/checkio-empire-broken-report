53e0e0962b5b84691b871bbdc0993db88e40fae2
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-16 17:21:55 +0300

    fix golf

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -1,5 +1,5 @@
 from checkio_referee import RefereeCodeGolf
-from checkio_referee.covercode import py_2_str
+from checkio_referee import covercodes
 
 import settings
 import settings_env
@@ -9,14 +9,14 @@ from tests import TESTS
 
 
 class Referee(RefereeCodeGolf):
-    DEFAULT_LENGTH = 150
+    DEFAULT_MAX_CODE_LENGTH = 150
     BASE_POINTS = 15
     TESTS = TESTS
     EXECUTABLE_PATH = settings.EXECUTABLE_PATH
     CURRENT_ENV = settings_env.CURRENT_ENV
     FUNCTION_NAME = "golf"
     ENV_COVERCODE = {
-        "python_2": py_2_str,
+        "python_2": covercodes.py_2_str,
         "python_3": None,
         "javascript": None
     }
